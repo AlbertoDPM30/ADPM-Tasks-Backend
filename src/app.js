d96efa3b2ef3e0fdc501aba2f4a739cbe3f0240a
AlbertoDPM30 <albertodpm2000@gmail.com>
2023-09-28 03:44:33 -0400

    Modificación de la ruta de origen del cliente

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    //origin: "http://localhost:5173",
+    origin: "https://adpm-tasks.netlify.app/",
     credentials: true,
   })
 );
